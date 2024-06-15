25524446ba4fe18d5e83d8f45261092719691396
sasanbrv <sasanbeyran@gmail.com>
2024-06-16 01:01:56 +0330

    complete-cart-to-buy

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,20 @@
+import { useContext } from "react";
+import { Products } from "../../data/Products";
+import { ShopContext } from "../../context/sohpContext";
+import Product from "../shop/Product";
+
+
 const Cart = () => {
+    const {cartItems} = useContext(ShopContext)
     return ( <>
-    <h1>cart</h1>
+    <h1>your cart items</h1>
+    <div className="row">
+        {Products.map((p)=> {
+            if( cartItems.some((i) => i.id === p.id && i.count > 0 ))
+            return <Product data={p}></Product>
+            else return <></>
+        })}
+    </div>
     </> );
 }
  
